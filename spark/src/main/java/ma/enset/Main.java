a29e7e468620e34752e1212a122bd0c666f879b1
elmehdi-elkari <elmehdielkari@gmail.com>
2023-11-27 16:16:28 +0000

    spark modifiction

--- spark/src/main/java/ma/enset/Main.java
+++ spark/src/main/java/ma/enset/Main.java
@@ -1,7 +1,7 @@
-package org.example;
+package ma.enset;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello spark!");
     }
 }
